3d72c4da8f5ca6124887253d649ffb309fc8fe77
simu <simu4learn@gmail.com>
2014-10-19 18:57:08 +0800

    大体写完，等待调试

--- src/EtermLibrary.py
+++ src/EtermLibrary.py
@@ -5,51 +5,86 @@ Created on Oct 16, 2014
 @author: czl
 '''
 
-import struct
-import socket
 from utils import hex_print
 import uuid
 import ConfigParser
 import codecs
+from matiplib import MATIP
 
 class EtermLibrary(object):
     '''
     classdocs
     '''
 
+    ver = 1
 
-    def __init__(self):
+    def __init__(self, host, port):
         '''
         Constructor
         '''
-        self.sock = socket.socket()
-        self.debug_level = 0
+        self.matip = MATIP(host, port)
+        self.ascus = []
+        self.currentSessionIndex = 0
         
     def __getMac(self):
         node = uuid.getnode()
         mac = uuid.UUID(int=node)
         return mac.hex[-12:]
         
-    def connect(self, host, port, user, password):
-        self.sock.connect((host, port))
+    def login(self, username, password):
+        ## 构造登录报文
+        content = chr(self.ver)    # version
+        content += chr(0xa2)       # 内容长度162个字节
         
-        buf = chr(0x1) + chr(0xa2)
-        buf += user.ljust(16, chr(0))
-        buf += password.ljust(32, chr(0))
+        assert(len(username) <= 16)
+        assert(len(password) <= 32)
+        content += username.ljust(16, chr(0))
+        content += password.ljust(32, chr(0))
         
         mac = self.__getMac()
-        buf += mac.ljust(12, chr(0))
-        
+        content += mac.ljust(12, chr(0))
         localip = self.sock.getsockname()[0]
-        buf += localip.ljust(16, chr(0x20))
-        buf += "3832010.000000"
-        buf = buf.ljust(162, chr(0))
+        content += localip.ljust(16, chr(0x20))
+        content += "3832010.000000"  #the magic code
+        content = content.ljust(162, chr(0))
         
-        self.sock.send(buf)
-        response = self.sock.recv(2048)
-        if self.debug_level > 0:
-            hex_print(response)
-
+        ## 登录并记录session id
+        self.matip.send(content)
+        h1s = set()
+        while 1:
+            resp = self.matip.getreply()
+            if ord(resp[0]) == 0 and ord(resp[1]) == len(resp):
+                self.sessionCount = ord(resp[4])
+                for i in range(self.sessionCount):
+                    ind = 5+5*i
+                    self.ascus.append(resp[ind:ind+5]) # save the H1 H2 A1 A2
+                    
+                    if resp[ind] != chr(0):
+                        h1s.add(id)
+                break
+        
+        ## ascus open
+        content = self.matip.getSessionOpenPacket(cd=4, styp=1, mpx=0, hdr=0, pres=2, h1=0, h2=0)
+        self.matip.send(content)
+        resp = self.matip.getreply()
+        
+        content = ''
+#         for h1 in (chr(0x0c), chr(0x29), chr(0x20)):
+        for h1 in list(h1s):
+            content += self.matip.getSessionOpenPacket(cd=4, styp=1, mpx=0, hdr=0, pres=2, h1=ord(h1), h2=0)
+        self.matip.send(content)
+        resp = self.matip.getreply()
+                
+    def changeToSession(self, index):
+        self.currentSessionIndex = index - 1
+    
+    def sendCmd(self, cmd):
+        data = "\x1b\x0b\x36\x20\x00\x0f\x1e"
+        data += cmd.strip()
+        data += "\x20"
+        content = self.matip.getDataPacket(self.ascus[self.currentSessionIndex], data)
+        resp = self.matip.send(content)
+        return resp
 
 if __name__ == "__main__":
     config = ConfigParser.ConfigParser()
@@ -60,9 +95,11 @@ if __name__ == "__main__":
     username = config.get("eterm", "username")
     password = config.get("eterm", "password")
     
-    el = EtermLibrary()    
-    el.debug_level = 1 
-    el.connect(server, port, username, password)   
+    el = EtermLibrary(server, port)    
+    el.login(username, password)
+    el.sendCmd("da:")
+    
+       
     
     
         
--- src/matiplib.py
+++ src/matiplib.py
@@ -7,8 +7,7 @@ Created on Oct 16, 2014
 
 import socket
 from sys import stderr
-import ConfigParser
-import codecs
+import struct
 
 
 MATIP_PORT = 350
@@ -27,6 +26,9 @@ class MATIPServerDisconnected(MATIPException):
     connecting it to a server.
     '''
 
+class MATIPResponseException(MATIPException):
+    """Base class for all exceptions that include an MATIP error code.
+    """
 
 class MATIP(object):
     '''
@@ -36,14 +38,15 @@ class MATIP(object):
     debuglevel = 0
     file = None
     default_port = 350
-
+    
+    ver = 1
+    
     def __init__(self, host='', port=0,
                  timeout=socket._GLOBAL_DEFAULT_TIMEOUT):
         '''
         Constructor
         '''
         self.timeout = timeout
-        self.esmtp_features = {}
         if host:
             self.connect(host, port)
             
@@ -85,28 +88,136 @@ class MATIP(object):
             raise MATIPServerDisconnected('Please run connect() first')
         
     def getreply(self):
-#         resp = []
         if self.file is None:
             self.file = self.sock.makefile('rb')
-        while 1:
-            try:
-                line = self.file.readline()
-            except socket.error as e:
-                self.close()
-                raise MATIPServerDisconnected("Connection unexpectedly closed: "
-                                              + str(e))
-            if line == '':
-                self.close()
-                raise MATIPServerDisconnected("Connection unexpectedly closed")
-            if self.debuglevel > 0:
-                print>>stderr, 'reply:', repr(line)
-            print line
-            break
+        try:
+            resp = self.file.readline()
+        except socket.error as e:
+            self.close()
+            raise MATIPServerDisconnected("Connection unexpectedly closed: "
+                                          + str(e))
+        if resp == '':
+            self.close()
+            raise MATIPServerDisconnected("Connection unexpectedly closed")
+        if self.debuglevel > 0:
+            print>>stderr, 'reply:', repr(resp)
+        return resp
             
-                
-    def login(self, username, password):
-        pass
     
+    def createPacket(self, version, cmd, content):
+        msg = chr(version)
+        msg += chr(cmd)
+        msg += struct.pack("!H", len(content)+4)
+        msg += content
+        return msg
+                
+    def getSessionOpenPacket(self, cd, styp, mpx, hdr, pres, h1, h2, ascus=[]):
+        '''
+        get the session open message
+        
+         0                   1                   2                   3
+         0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |0|0|0|0|0| Ver |1|1 1 1 1 1 1 0|            length             |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |0 0|0 1|0| CD  | STYP  |0 0 0 0| RFU           |MPX|HDR| PRES. |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |       H1      |      H2       |              RFU              |
+        |-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |    Reserved   |              RFU              |  Nbr of ASCUs |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        | Nbr of ASCUs  |     ASCU list (opt)                           |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        
+        CD
+        This field specifies the Coding
+        000 : 5 bits (padded baudot)
+        010 : 6 bits (IPARS)
+        100 : 7 bits (ASCII)
+        110 : 8 bits (EBCDIC)
+        xx1 : R.F.U
+        
+        STYP
+        This is the traffic subtype (type being TYPE A).
+        0001 : TYPE A Conversational
+        
+        MPX
+        This flag specifies the multiplexing used within the TCP session.
+        Possible values are:
+        00 : Group of ASCU with 4 bytes identification per ASCU (H1H2A1A2)
+        01 : Group of ASCUs with 2 bytes identification per ASCU (A1A2)
+        10 : single ASCU inside the TCP session.
+        
+        HDR
+        This field specifies which part of the airline’s specific address is
+        placed ahead of the message texts transmitted over the session.
+        Possible values are:
+        00 : ASCU header = H1+H2+A1+A2
+        01 : ASCU Header = A1+A2
+        10 : No Header
+        11 : Not used
+        The MPX and HDR must be coherent. When ASCUs are multiplexed, the data
+        must contain the ASCU identification. The table below summarizes the
+        allowed combinations:
+        +-----------------------+
+        | MPX |  00 |  01 |  10 |
+        +-----------------------+
+        | HDR |                 |
+        | 00  |  Y  |  Y  |  Y  |
+        | 01  |  N  |  Y  |  Y  |
+        | 10  |  N  |  N  |  Y  |
+        +-----------------------+
+        
+        PRES
+        This field indicates the presentation format
+        0001 : P1024B presentation
+        0010 : P1024C presentation
+        0011 : 3270 presentation
+        
+        H1 H2
+        These fields can logically identify the session if MPX is not equal to
+        00. When this field is not used, it must be set to 0. If used in
+        session (MPX <> 0) with HDR=00, H1H2 in data packet must have the same
+        value as set in SO command.
+        
+        Nbr of ASCUs
+        Nbr_of_ASCUs field is mandatory and gives the number of ASCUs per
+        session. A 0 (zero) value means unknown. In this case the ASCU list is
+        not present in the ‘Open Session’ command and must be sent by the
+        other end in the ‘Open Confirm’ command.
+        
+        ASCU LIST
+        Contains the list of identifier for each ASCU. If MPX=00 it has a
+        length of four bytes (H1H2A1A2) for each ASCU, otherwise it is two
+        bytes (A1A2).        
+        '''
+        content = chr(1<<4 | cd)
+        content += chr(styp<<4)
+        content += chr(0) # RFU
+        content += chr(mpx<<6 | hdr<<4 | pres)
+        content += chr(h1)
+        content += chr(h2)
+        content += ''.ljust(2, chr(0)) # RFU
+        content += chr(0) # Reserved
+        content += ''.ljust(2, chr(0)) # RFU
+        content += struct.pack("!H", len(ascus))
+        
+        if mpx == 0:
+            fmt = "4B"  # 4个字节(H1H2A1A2)
+        else:
+            fmt = "2B"  # 2个字节(A1A2)
+        for ascu in ascus:
+            content += struct.pack(fmt, *ascu)
+    
+        return self.createPacket(self.ver, chr(0xFE), content)
+    
+    def getDataPacket(self, ascu, data):
+        content = ascu[:3]+chr(1)+ascu[3:]
+        content += chr(0x70)
+        content += data
+        content += chr(0x3)
+        return self.createPacket(self.ver, chr(0), content)
+            
     def close(self):
         if self.file:
             self.file.close()
@@ -118,7 +229,8 @@ class MATIP(object):
     
 
 if __name__ == "__main__":
-    pass    
+    import ConfigParser
+    import codecs    
     config = ConfigParser.ConfigParser()
     config.readfp(codecs.open('config.cgi', 'r', 'utf-8-sig'))
     
