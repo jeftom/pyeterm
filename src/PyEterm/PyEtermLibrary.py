eb3289851c8eb103239234c22dbde8ebb34da8b2
simu4learn <simu4learn@gmail.com>
2014-10-28 17:42:54 +0800

    配合robot framework进行修改

--- src/PyEterm/PyEtermLibrary.py
+++ src/PyEterm/PyEtermLibrary.py
@@ -5,16 +5,16 @@ Created on Oct 16, 2014
 @author: czl
 '''
 
-from utils import hex_print
+from PyEterm.utils import hex_print
 import uuid
 import ConfigParser
 import codecs
 import os
-from matiplib import MATIP
+from PyEterm.matiplib import MATIP
 import re
 import logging
 
-class EtermLibrary(object):
+class PyEtermLibrary(object):
     '''
     classdocs
     '''
@@ -32,7 +32,10 @@ class EtermLibrary(object):
         self.currentSessionIndex = 0
         
     def connect(self, host, port):
+        if not isinstance(port, int):
+            port = int(port)
         self.matip.connect(host, port)
+        return "connect success"
         
     def __getMac(self):
         node = uuid.getnode()
@@ -40,6 +43,11 @@ class EtermLibrary(object):
         return mac.hex[-12:]
         
     def login(self, username, password):
+        if isinstance(username, unicode):
+            username = username.encode("ascii", "ignore")
+        if isinstance(password, unicode):
+            password = password.encode("ascii", "ignore")
+        
         ## 构造登录报文
         content = chr(self.ver)  # version
         content += chr(0xa2)  # 内容长度162个字节
@@ -85,8 +93,11 @@ class EtermLibrary(object):
         self.matip.send(content)
         resp = self.matip.getreply()
         resp = self.matip.getreply()
+        return "login success"
                 
     def changeToSession(self, index):
+        if not isinstance(index, int):
+            index = int(index)
         self.currentSessionIndex = index - 1
     
     def eterm_print(self, cmd, text):
@@ -96,6 +107,7 @@ class EtermLibrary(object):
         content = self.eterm_print_pattern.sub(r"\1\r", content)
         content = content.replace('\x0d', os.linesep)
         print content
+        return content
         
     def sendCmd(self, cmd):
         data = "\x02\x1b\x0b\x20\x20\x00\x0f\x1e"
@@ -108,21 +120,21 @@ class EtermLibrary(object):
         self.matip.send(content)
         
         resp = self.matip.getreply()
-        self.eterm_print(cmd, resp)
+        return self.eterm_print(cmd, resp)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     logging.info("hello")
     
     config = ConfigParser.ConfigParser()
-    config.readfp(codecs.open('config.cgi', 'r', 'utf-8-sig'))
+    config.readfp(codecs.open('../config.cgi', 'r', 'utf-8-sig'))
     
     server = config.get("eterm", "server")
     port = config.getint("eterm", "port")
-    username = config.get("eterm", "username").encode('ascii', 'ignore')
-    password = config.get("eterm", "password").encode('ascii', 'ignore')
+    username = config.get("eterm", "username")
+    password = config.get("eterm", "password")
     
-    el = EtermLibrary()
+    el = PyEtermLibrary()
     el.connect(server, port)   
     el.login(username, password)
     resp = el.sendCmd("da")
